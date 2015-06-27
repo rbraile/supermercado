0d814e6215ab4d3988f165173a60ed0c3418ca6c
Roberto <roberto.braile@gmail.com>
2015-06-27 12:16:37 -0300

    agregado de las clase carrito y la clase test

--- src/main/java/tallerweb/supermercado/modelo/Carrito.java
+++ src/main/java/tallerweb/supermercado/modelo/Carrito.java
@@ -1,83 +1,107 @@
 package tallerweb.supermercado.modelo;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-
 public class Carrito {
 
-    private static Carrito instance = new Carrito();
-    private List<Producto> productos = new LinkedList<Producto>();
-    private List<Descuento> descuentos = new LinkedList<Descuento>();
+	private static Carrito instance = new Carrito();
+	private List<Producto> productos = new LinkedList<Producto>();
+	private List<Descuento> descuentos = new LinkedList<Descuento>();
 
-    private Carrito() {
-    }
+	private Carrito() {
+	}
 
-    public static Carrito getInstance() {
-        return instance;
-    }
+	public static Carrito getInstance() {
+		return instance;
+	}
 
-    /**
-     * Elimina todos los productos del carrito.<br>
-     */
-    public void vaciar() {
-        // Implementar
-    }
+	/**
+	 * Elimina todos los productos del carrito.<br>
+	 */
+	public void vaciar() {
+		this.productos.clear();
+	}
+	
+	public void vaciarDescuentos() {
+		this.descuentos.clear();
+	}
 
-    /**
-     * Agrega un producto al carrito.<br>
-     * 
-     * @param ingrediente
-     */
-    public void agregarProducto(Producto producto) {
+	/**
+	 * Agrega un producto al carrito.<br>
+	 * 
+	 * @param ingrediente
+	 */
+	public void agregarProducto(Producto producto) {
 		this.productos.add(producto);
-    }
+	}
 
-    /**
-     * Permite agregar un descuento que será aplicado al carrito.<br>
-     * 
-     * @param descuento
-     */
-    public void aplicarDescuento(Descuento descuento) {
-        // Implementar
-    }
+	/**
+	 * Permite agregar un descuento que será aplicado al carrito.<br>
+	 * 
+	 * @param descuento
+	 */
+	public void aplicarDescuento(Descuento descuento) {
+		this.descuentos.add(descuento);
+	}
 
-    /**
-     * Lista todos los productos que forman parte .del carrito.<br>
-     * 
-     * @return
-     */
-    public List<Producto> verProductos() {
-        return this.productos;
-    }
+	/**
+	 * Lista todos los productos que forman parte .del carrito.<br>
+	 * 
+	 * @return
+	 */
+	public List<Producto> verProductos() {
+		return this.productos;
+	}
+	
+	public List<Descuento> verDescuentos() {
+		return this.descuentos;
+	}
 
-    /**
-     * Devuelve el precio total del carrito aplicando descuentos si los hubiese,
-     * primero aplica los descuentos de monto y luego los de porcentaje.<br>
-     * 
-     * @return
-     */
-    public Double total() {
-    	return 1.0;
-    }
+	/**
+	 * Devuelve el precio total del carrito aplicando descuentos si los hubiese,
+	 * primero aplica los descuentos de monto y luego los de porcentaje.<br>
+	 * 
+	 * @return
+	 */
+	public Double total() {
+		return this.totalSinDescuentos() - this.totalAhorros();
+	}
 
-    /**
-     * Devuelve el precio sin descuentos del carrito.<br>
-     * 
-     * @return
-     */
-    public Double totalSinDescuentos() {
-        // Implementar
-        return null;
-    }
+	/**
+	 * Devuelve el precio sin descuentos del carrito.<br>
+	 * 
+	 * @return
+	 */
+	public Double totalSinDescuentos() {
+		Double total = 0.00;
 
-    /**
-     * Devuelve el dinero ahorrado gracias a los descuentos.<br>
-     * 
-     * @return
-     */
-    public Double totalAhorros() {
-        // Implementar
-        return null;
-    }
+		for (Iterator<Producto> it = this.verProductos().iterator(); it.hasNext();) {
+			Producto each = it.next();
+			total += each.getPrecio();
+		}
+		
+		return total;
+	}
+
+	/**
+	 * Devuelve el dinero ahorrado gracias a los descuentos.<br>
+	 * 
+	 * @return
+	 */
+	public Double totalAhorros() {
+		Double totalPorcentaje = 0.00;
+		Double totalMonto = 0.00;
+		Double totalAPagar = this.totalSinDescuentos();
+		
+		for (Iterator<Descuento> it = this.descuentos.iterator(); it.hasNext();) {
+			Descuento each = it.next();
+			totalMonto += (each.getMonto() != null )? each.getMonto(): 0.00;
+			totalPorcentaje += (each.getPorcentaje() != null )? each.getPorcentaje(): 0.00;
+		}
+		totalAPagar -= totalMonto;
+		totalPorcentaje = (totalPorcentaje>0)? (totalAPagar * totalPorcentaje)/100 : 0.00;
+		return totalMonto + totalPorcentaje;
+	}
 }
--- src/test/java/tallerweb/supermercado/modelo/ProductoTest.java
+++ src/test/java/tallerweb/supermercado/modelo/ProductoTest.java
@@ -1,54 +1,190 @@
 package tallerweb.supermercado.modelo;
 
+import java.util.Iterator;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ProductoTest {
-    private Carrito carrito;
-    
-    @Before
-    public void init(){
-        carrito = Carrito.getInstance(); 
-    }
+	private Carrito carrito;
 
-//    @Test
-//    public void testVaciar() {
-//        // Implementar
-//    }
+	@Before
+	public void init() {
+		carrito = Carrito.getInstance();
+	}
 
-    @Test
-    public void testAgregarProducto() {
-    	
+	@Test
+	public void testVaciar() {
 		Producto p1 = new Producto();
 		p1.setNombre("Harina");
 		p1.setPrecio(12.01);
 		carrito.agregarProducto(p1);
-		Assert.assertTrue(1 == carrito.verProductos().size());
-    }
+		carrito.vaciar();
+		Assert.assertTrue(0 == carrito.verProductos().size());
+	}
 
-//    @Test
-//    public void testAplicarDescuento() {
-//        // Implementar
-//    }
-//
-//    @Test
-//    public void testVerProductos() {
-//        // Implementar
-//    }
-//
-//    @Test
-//    public void testTotal() {
-//        // Implementar
-//    }
-//
-//    @Test
-//    public void testTotalSinDescuentos() {
-//        // Implementar
-//    }
-//
-//    @Test
-//    public void testTotalAhorros() {
-//        // Implementar
-//    }
+	@Test
+	public void testAgregarProducto() {
+		carrito.vaciar();
+		Producto p1 = new Producto();
+		p1.setNombre("Harina");
+		p1.setPrecio(20.00);
+		carrito.agregarProducto(p1);
+		Assert.assertTrue(1 == carrito.verProductos().size());
+	}
+
+	@Test
+	public void testAplicarDescuento() {
+		carrito.vaciar();
+		Producto p2 = new Producto();
+		p2.setNombre("Harinas");
+		p2.setPrecio(20.00);
+		carrito.agregarProducto(p2);
+
+		Descuento d1 = new Descuento();
+		d1.setPorcentaje(10.00);
+		carrito.aplicarDescuento(d1);
+
+		Assert.assertEquals(2.00, carrito.totalAhorros(), 0.1);
+
+	}
+
+	@Test
+	public void testVerProductos() {
+		carrito.vaciar();
+
+		Producto p1 = new Producto();
+		p1.setNombre("harinas");
+		p1.setPrecio(20.00);
+		carrito.agregarProducto(p1);
+
+		Producto p2 = new Producto();
+		p2.setNombre("papa");
+		p2.setPrecio(30.00);
+		carrito.agregarProducto(p2);
+
+		Producto p3 = new Producto();
+		p3.setNombre("pollo");
+		p3.setPrecio(130.00);
+		carrito.agregarProducto(p3);
+
+		int i=0;
+		for (Iterator<Producto> it = carrito.verProductos().iterator(); it.hasNext();) {
+			Producto each = it.next();
+			switch (i){
+	            case 0:
+	            	 Assert.assertTrue("harinas".equals(each.getNombre()));
+	                break;
+	            case 1:
+	                Assert.assertTrue("papa".equals(each.getNombre()));
+	                break;
+	            case 2:
+	                Assert.assertTrue("pollo".equals(each.getNombre()));
+	                break;
+            }
+			i++;
+		}
+	}
+
+	@Test
+	public void testTotal() {
+		carrito.vaciar();
+		carrito.vaciarDescuentos();
+
+		Producto p1 = new Producto();
+		p1.setNombre("Harinas");
+		p1.setPrecio(20.00);
+		carrito.agregarProducto(p1);
+
+		Producto p2 = new Producto();
+		p2.setNombre("papa");
+		p2.setPrecio(30.00);
+		carrito.agregarProducto(p2);
+
+		Producto p3 = new Producto();
+		p3.setNombre("pollo");
+		p3.setPrecio(130.00);
+		carrito.agregarProducto(p3);
+
+		Descuento d1 = new Descuento();
+		d1.setMonto(10.00);
+		carrito.aplicarDescuento(d1);
+
+		Descuento d4 = new Descuento();
+		d4.setMonto(20.00);
+		carrito.aplicarDescuento(d4);
+
+		Descuento d2 = new Descuento();
+		d2.setPorcentaje(10.00);
+		carrito.aplicarDescuento(d2);
+
+		Descuento d3 = new Descuento();
+		d3.setPorcentaje(20.00);
+		carrito.aplicarDescuento(d3);
+
+		Assert.assertEquals(105.00, carrito.total(), 0.1);
+	}
+
+	@Test
+	public void testTotalSinDescuentos() {
+		carrito.vaciar();
+
+		Producto p1 = new Producto();
+		p1.setNombre("Harinas");
+		p1.setPrecio(20.00);
+		carrito.agregarProducto(p1);
+
+		Producto p2 = new Producto();
+		p2.setNombre("papa");
+		p2.setPrecio(10.00);
+		carrito.agregarProducto(p2);
+
+		Producto p3 = new Producto();
+		p3.setNombre("pollo");
+		p3.setPrecio(100.00);
+		carrito.agregarProducto(p3);
+
+		Assert.assertEquals(130.00, carrito.totalSinDescuentos(), 0.1);
+
+	}
+
+	@Test
+	public void testTotalAhorros() {
+		carrito.vaciar();
+		carrito.vaciarDescuentos();
+
+		Producto p1 = new Producto();
+		p1.setNombre("Harinas");
+		p1.setPrecio(20.00);
+		carrito.agregarProducto(p1);
+
+		Producto p2 = new Producto();
+		p2.setNombre("papa");
+		p2.setPrecio(30.00);
+		carrito.agregarProducto(p2);
+
+		Producto p3 = new Producto();
+		p3.setNombre("pollo");
+		p3.setPrecio(130.00);
+		carrito.agregarProducto(p3);
+
+		Descuento d1 = new Descuento();
+		d1.setMonto(10.00);
+		carrito.aplicarDescuento(d1);
+
+		Descuento d4 = new Descuento();
+		d4.setMonto(20.00);
+		carrito.aplicarDescuento(d4);
+
+		Descuento d2 = new Descuento();
+		d2.setPorcentaje(10.00);
+		carrito.aplicarDescuento(d2);
+
+		Descuento d3 = new Descuento();
+		d3.setPorcentaje(20.00);
+		carrito.aplicarDescuento(d3);
+
+		Assert.assertEquals(75.00, carrito.totalAhorros(), 0.1);
+	}
 }
